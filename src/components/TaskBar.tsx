e38bd3c991c7b5f19b2490a2faea055eff5c2591
thomashanna <thanna96@gmail.com>
2022-03-11 23:56:06 -0500

    added time

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,12 +1,26 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import startIcon from "../files/icons/start_main.0.jpg";
 
 export const TaskBar: FC = function () {
   const [menuActive, setMenuActive] = useState<boolean>(false);
+  const [time, setTime] = useState<string>("");
 
+  useEffect(() => {
+    setInterval(
+      () =>
+        setTime(
+          new Date().toLocaleTimeString("en-us", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })
+        ),
+      1000
+    );
+    return (): void => clearInterval();
+  }, []);
   return (
     <div
-      className={"w-full m-0 absolute bottom-0"}
+      className={"w-full m-0 absolute bottom-0 py-0.5"}
       style={{ background: "#C0C0C0", height: "40px" }}
     >
       <button
@@ -16,11 +30,15 @@ export const TaskBar: FC = function () {
         <img
           src={startIcon}
           alt={"start menu icon"}
-          style={{ height: "38px" }}
+          style={{ height: "35px" }}
         />
       </button>
+      <div
+        style={{ height: "33px" }}
+        className={"float-right border border-black shadow-inner px-5"}
+      >
+        <p className={"mb-0 mt-0.5 text-lg"}>{time}</p>
+      </div>
     </div>
   );
 };
-
-export default TaskBar;
